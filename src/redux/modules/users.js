c2fae20521c7f77ba45a5d1e49c38718e8991649
leejpsd <wndvy7690@nate.com>
2022-08-17 20:00:09 +0900

    done

--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -2,52 +2,30 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { useCookies } from 'react-cookie'
 
-// export const __getUsers = createAsyncThunk(
-//   "users/getUsers",
-//   async (payload, thunkAPI) => {
-//     try {
-//       const data = await axios.get("http://localhost:3001/todos");
-//       return thunkAPI.fulfillWithValue(data.data);
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error);
-//     }
-//   }
-// );
 
 export const __postUsers = createAsyncThunk(
   "users/postUsers",
   async (userInfo, thunkAPI) => {
-    try {
-      const data = await axios.post("http://3.36.64.146/user/login", userInfo)
+    try { 
+      
+      const data = await axios.post("http://3.34.98.245/user/login", userInfo)
       .then((response) => {
+        
         console.log(response)
-        
-        const { accessToken } = response.data
-        
-        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
 
-        // const accessToken = response.data.token;
-        // setCookie("is_login", `${accessToken}`);
-        // document.location.href = "/";
-        // localStorage.setItem('refreshToken',response.data['refreshTokentoken'])
+        if (response.data.success === true) {
+          return alert('로그인성공!'); 
+        }
+        window.localStorage.setItem('login-token', response.headers.accesstoken);
+      
+
         // document.location.href = "/";
       })
       return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
-  },
-  // async (userInfo, thunkAPI) => {
-  //   try {
-  //     onSilentRefresh = () => {
-  //     await axios.post('/silent-refresh', userInfo)
-  //     .then(onLoginSuccess)
-  //   }
-  //     return thunkAPI.fulfillWithValue(data.data);
-  //   } catch (error) {
-  //     return thunkAPI.rejectWithValue(error);
-  //   }
-  // }
+  }
 
 );
 
@@ -64,21 +42,6 @@ export const usersSlice = createSlice({
   reducers: {
   },
   extraReducers: {
-    // [__getTodos.pending]: (state) => {
-    //   state.isLoading = true;
-    // },
-    // [__getTodos.fulfilled]: (state, action) => {
-    //   state.isLoading = false;
-    //   state.todos = action.payload;
-    // },
-    // [__getTodos.rejected]: (state, action) => {
-    //   state.isLoading = false;
-    //   state.error = action.payload;
-    // },
-    // [__postUsers.fulfilled]: (state, action) => {
-    //   state.isLoading = false;
-    //   state.todos = action.payload;
-    // },
   },
 });
 
